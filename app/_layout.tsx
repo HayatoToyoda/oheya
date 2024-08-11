226a3dbde6a9ef0127b4384438f794a76d429e9d
Tatsuya3327 <tatsu3327@gmail.com>
2024-08-11 18:37:46 -0500

    feature(userProfile): add link to user profile pages

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,6 +7,7 @@ export default function RootLayout() {
       <Stack.Screen name="home" options={{ title: 'Home Screen' }} />
       <Stack.Screen name="about" options={{ title: 'About' }} />
       <Stack.Screen name="itemsForALine" options={{ title: 'Items For A Line' }} />
+      <Stack.Screen name="profile" options={{ title: 'User profiles' }} />
     </Stack>
   );
 }
