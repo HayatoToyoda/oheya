844f446509b35fe97bf150c527aa013960ac27be
Tatsuya3327 <tatsu3327@gmail.com>
2024-08-18 13:54:44 -0500

    fix(AuthContextProvider): added async storage to persist login state between sessions

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,6 +1,8 @@
+// @ts-ignore
+import { initializeAuth, getReactNativePersistence } from "firebase/auth";
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
+import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
 
 // Optionally import the services that you want to use
 // import {...} from "firebase/auth";
@@ -24,7 +26,9 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 // Initialize Firebase Authentication and get a reference to the service
-const auth = getAuth(app);
+const auth = initializeAuth(app, {
+    persistence: getReactNativePersistence(ReactNativeAsyncStorage),
+});
 
 // Initialize Firebase Realtime Database
 const database = getDatabase(app);
